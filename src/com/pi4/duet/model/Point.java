1d96611f6f4c528cafc92907272da6149690c5c9
Evan JOFFRIN <evan.joffrin@interexa.fr>
2023-02-04 22:32:17 +0100

    Modifications coordonnées dans Point en double; remplissage de la classe Obstacle pour permettre la rotation et le déplacement de ce dernier.

--- src/com/pi4/duet/model/Point.java
+++ src/com/pi4/duet/model/Point.java
@@ -2,17 +2,17 @@ package com.pi4.duet.model;
 
 public class Point {
 
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 	
-	public Point(int x, int y) {
+	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 	
-	public int getX() { return x; }
-	public int getY() { return y; }
+	public double getX() { return x; }
+	public double getY() { return y; }
 	
-	public void setX(int x) { this.x = x; }
-	public void setY(int y) { this.y = y; }
+	public void setX(double d) { this.x = d; }
+	public void setY(double d) { this.y = d; }
 }
